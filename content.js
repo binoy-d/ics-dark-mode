6d6c70fbf91d614454e27d13233d9e159ac10cee
Daniel Binoy <dbinoy15@gmail.com>
2020-04-07 21:42:50 -0700

    d

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 window.onload = function(){
   console.log("hello!");
   var link = document.createElement( "link" );
-  link.href = "https://www.binoy.co/files/dark_mode.css"
+  link.href = "https://binoy.co/files/dark_mode.css"
   link.type = "text/css";
   link.rel = "stylesheet";
   document.getElementsByTagName( "head" )[0].appendChild( link );
