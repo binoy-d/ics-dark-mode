b2411f0db795d7a8afaedaff5e7eb8178610050e
Daniel Binoy <dbinoy15@gmail.com>
2020-04-07 21:37:09 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,9 +1,8 @@
 window.onload = function(){
   console.log("hello!");
-
-  var paras = document.getElementsByTagName("p")
-  for(var i = 0;paras.length;i++ ){
-    this.console.log(paras[i].style.color="blue");
-  }
-  
+  var link = document.createElement( "link" );
+  link.href = "https://www.binoy.co/files/dark_mode.css"
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  document.getElementsByTagName( "head" )[0].appendChild( link );
 }
