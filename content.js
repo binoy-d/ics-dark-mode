121a9fecbf7519c20905e89d44c4c84b69628835
Daniel Binoy <dbinoy15@gmail.com>
2020-04-07 21:31:46 -0700

    intial

---
+++ content.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+  console.log("hello!");
+
+  var paras = document.getElementsByTagName("p")
+  for(var i = 0;paras.length;i++ ){
+    this.console.log(paras[i].style.color="blue");
+  }
+  
+}
